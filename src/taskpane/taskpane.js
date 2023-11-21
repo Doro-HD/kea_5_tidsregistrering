121131b3bd0e12ba339fc012053e4bf2da245e16
Doro-HD <Doro-HD@DESKTOP-FLIBGAM>
2023-11-21 10:23:31 +0100

    Not sure

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,8 +10,18 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+
+    document.getElementById("call").onclick = test;
   }
 });
+
+async function test() {
+  const res = await fetch("http://localhost:5040/Test")
+  const data = await res.json()
+
+  const node = document.querySelector("#item-subject")
+  node.textContent = data.testName
+}
 
 export async function run() {
   // Get a reference to the current message
