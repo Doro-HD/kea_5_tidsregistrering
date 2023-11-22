f35d3e9733b04452e9794999af75b822a3b0586c
Victor-Desktop <vict702k@stud.kea.dk>
2023-11-22 13:58:20 +0100

    Added javascript fetch call and httpErrorResponse handler to our taskpane.js file

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -15,56 +15,84 @@ Office.onReady((info) => {
 });
 
 export async function run() {
+
+
+  const projectId = document.querySelector("input#project-id").value
+
+  try {
+    const data = await fetch('https://timereg-api.azurewebsites.net/test', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'text/plain', // Specify the content type as plain text
+        },
+        body: projectId
+    }).then(res => handleHttpErrors(res))
+
+    document.getElementById("returned-message-backend").innerHTML = "";
+    alert("Projekt Id added!")
+    console.log("Added")
+} catch (err) {
+    //document.getElementById("returned-message-backend").innerHTML = (err.apiError.response);
+    console.error(err)
+
+}
+//DOMPurify.sanitize
+
+
   // Get a reference to the current message
  // const item = Office.context.mailbox.item;
 
   // Write message property value to the task pane
-  document.getElementById("returned-message-backend").innerHTML = "<b>Status:</b> Gemt! (Hardcoded) <br/>";
+  //document.getElementById("returned-message-backend").innerHTML = "<b>Status:</b> Gemt! (Hardcoded) <br/>";
 
-
-
+}
 /*
-Export function taget fra vores 3.semester.
+
+//Export function taget fra vores 3.semester.
 async function register() {
 
-    const usernameInput = document.querySelector("input#username").value
-    const passwordInput = document.querySelector("input#password").value
+    const projectId = document.querySelector("input#project-id").value
 
-    let headers = new Headers()
-    headers.append("Content-Type", "application/json; charset=utf-8")
-    headers.append("Accept", "application/json")
+    //let headers = new Headers()
+    //headers.append("Content-Type", "application/json; charset=utf-8")
+    //headers.append("Accept", "application/json")
 
-    const jsonBody = JSON.stringify({username: usernameInput, password: passwordInput})
+    //const jsonBody = JSON.stringify({username: usernameInput, password: passwordInput})
     
     try {
-        const data = await fetch(baseURL + "/api/register", {
+        const data = await fetch("timereg-api.azurewebsites.net/test/", {
             method: 'post',
-            headers: headers,
-            body: jsonBody
+            headers: {
+              'Content-Type': 'text/plain', // Specify the content type as plain text
+            },
+            body: projectId
         }).then(res => handleHttpErrors(res))
 
-        document.getElementById("error-on-register").innerHTML = "";
+        document.getElementById("returned-message-backend").innerHTML = "";
         window.router.navigate("/")
-        alert("Bruger oprettet!")
-        console.log("Registreret")
+        alert("Projekt Id added!")
+        console.log("Added")
     } catch (err) {
-        document.getElementById("error-on-register").innerHTML = DOMPurify.sanitize(err.apiError.response);
+        document.getElementById("returned-message-backend").innerHTML = DOMPurify.sanitize(err.apiError.response);
         console.error(err)
     
     }
 
+
+
+
+}
 */
 
-
-
-
-
-}
-
-export async function testFunction(event) {
-  // Get a reference to the current message
-  const item = Office.context.mailbox.item;
-
-  // Write message property value to the task pane
-  document.getElementById(item.body).innerHTML = "Hello";
-}
+//Error function taget fra vores 3.semester.
+    async function handleHttpErrors(res) {
+      console.log("I httperrorfunctionen" + res)
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      const error = new Error(errorResponse.message)
+      error.apiError = errorResponse
+      throw error
+    }
+    
+    return res.json()
+  }
