1ae761b6d9a4ef92b33d9bae93301248fdce819d
Victor-Desktop <vict702k@stud.kea.dk>
2023-11-22 12:28:04 +0100

    Added new text boxes to input fields in taskpane. Inserted fetch function from third semester project.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -16,10 +16,49 @@ Office.onReady((info) => {
 
 export async function run() {
   // Get a reference to the current message
-  const item = Office.context.mailbox.item;
+ // const item = Office.context.mailbox.item;
 
   // Write message property value to the task pane
-  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+  document.getElementById("returned-message-backend").innerHTML = "<b>Status:</b> Gemt! (Hardcoded) <br/>";
+
+
+
+/*
+Export function taget fra vores 3.semester.
+async function register() {
+
+    const usernameInput = document.querySelector("input#username").value
+    const passwordInput = document.querySelector("input#password").value
+
+    let headers = new Headers()
+    headers.append("Content-Type", "application/json; charset=utf-8")
+    headers.append("Accept", "application/json")
+
+    const jsonBody = JSON.stringify({username: usernameInput, password: passwordInput})
+    
+    try {
+        const data = await fetch(baseURL + "/api/register", {
+            method: 'post',
+            headers: headers,
+            body: jsonBody
+        }).then(res => handleHttpErrors(res))
+
+        document.getElementById("error-on-register").innerHTML = "";
+        window.router.navigate("/")
+        alert("Bruger oprettet!")
+        console.log("Registreret")
+    } catch (err) {
+        document.getElementById("error-on-register").innerHTML = DOMPurify.sanitize(err.apiError.response);
+        console.error(err)
+    
+    }
+
+*/
+
+
+
+
+
 }
 
 export async function testFunction(event) {
