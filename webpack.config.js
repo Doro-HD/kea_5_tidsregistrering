cc007217509c170bd51ccd267626732ba0f6bfdd
Doro-HD <Doro-HD@DESKTOP-FLIBGAM>
2023-12-07 11:57:33 +0100

    Correct storage account

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://coderbois.blob.core.windows.net/add-in/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://timeit.blob.core.windows.net/add-in/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
